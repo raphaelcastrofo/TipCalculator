3933eb031b5ddceaced0f441ab2df3cf23aea889
raphaelcastrofo <raphaelcastrofo@gmail.com>
2024-08-01 22:09:30 +0200

    added some error tratment and the spiner

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,16 +1,112 @@
 package com.example.tipcalculator
 
 import android.app.TaskStackBuilder
+import android.health.connect.datatypes.units.Percentage
 import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var percentage: Int = 0
+
+
+
+        binding.rbOptionOne.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                percentage = 10
+            }
+        }
+
+        binding.rbOptionTwo.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                percentage = 15
+            }
+        }
+
+        binding.rbOptionThree.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                percentage = 20
+            }
+        }
+
+        val totalTableTemp = binding.tieTotal.text
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.num_people,
+            android.R.layout.simple_spinner_item
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinner.adapter = adapter
+
+        var numOfPeopleSelected = 0
+        binding.spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                numOfPeopleSelected = position
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        binding.btnDone.setOnClickListener {
+
+
+
+            if (totalTableTemp != null) {
+                if (totalTableTemp.isEmpty() == true ) {
+                    Snackbar.make(binding.tieTotal, "preencha todos os campos", Snackbar.LENGTH_LONG)
+                        .show()
+                } else {
+
+                    val totalTable: Float = totalTableTemp.toString().toFloat()
+                    val nPeople: Int = numOfPeopleSelected
+
+                    val totalTemp = totalTable / nPeople
+                    val tips = totalTemp * percentage / 100
+                    val totalWithTip = totalTemp + tips
+                    binding.tvResult.text = "Total with tips: $totalWithTip"
+
+                }
+            }
+
+            binding.btnClean.setOnClickListener {
+                binding.tvResult.text = ""
+                binding.tieTotal.setText("")
+                binding.rbOptionOne.isChecked = false
+                binding.rbOptionTwo.isChecked = false
+                binding.rbOptionThree.isChecked = false
+
+
+            }
+
+        }
     }
 }
